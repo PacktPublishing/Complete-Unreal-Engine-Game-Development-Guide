29e9245a1fde84d7f5d4def014f3ad9f5410e538
ashishjPackt <56333060+ashishjPackt@users.noreply.github.com>
2019-11-12 19:47:27 +0530

    c20-c1

---
+++ Chapter-20/Source/Mastering/MasteringCharacter.cpp
@@ -0,0 +1,338 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "MasteringCharacter.h"
+#include "Animation/AnimInstance.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/InputSettings.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
+#include "MotionControllerComponent.h"
+#include "XRMotionControllerBase.h" // for FXRMotionControllerBase::RightHandSourceId
+#include "Inventory/MasteringInventory.h"
+#include "Inventory/MasteringWeapon.h"
+#include "UI/MasteringHUD.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
+
+//////////////////////////////////////////////////////////////////////////
+// AMasteringCharacter
+
+AMasteringCharacter::AMasteringCharacter()
+{
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
+
+	// set our turn rates for input
+	BaseTurnRate = 45.f;
+	BaseLookUpRate = 45.f;
+
+	// Create a CameraComponent	
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->RelativeLocation = FVector(-39.56f, 1.75f, 64.f); // Position the camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	Mesh1P->SetOnlyOwnerSee(true);
+	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
+	Mesh1P->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
+
+	// Create VR Controllers.
+	R_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("R_MotionController"));
+	R_MotionController->MotionSource = FXRMotionControllerBase::RightHandSourceId;
+	R_MotionController->SetupAttachment(RootComponent);
+	L_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("L_MotionController"));
+	L_MotionController->SetupAttachment(RootComponent);
+
+	Inventory = CreateDefaultSubobject<UMasteringInventory>(TEXT("Inventory"));
+
+	// Uncomment the following line to turn motion controllers on by default:
+	//bUsingMotionControllers = true;
+}
+
+void AMasteringCharacter::BeginPlay()
+{
+	// Call the base class  
+	Super::BeginPlay();
+
+	InitializeInventoryHUD();
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Input
+
+void AMasteringCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	// set up gameplay key bindings
+	check(PlayerInputComponent);
+
+	// Bind jump events
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	// Bind fire event
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AMasteringCharacter::OnFire);
+
+	// Enable touchscreen input
+	EnableTouchscreenMovement(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AMasteringCharacter::OnResetVR);
+
+	// Bind movement events
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMasteringCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMasteringCharacter::MoveRight);
+
+	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
+	// "turn" handles devices that provide an absolute delta, such as a mouse.
+	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("TurnRate", this, &AMasteringCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AMasteringCharacter::LookUpAtRate);
+
+	// Cycling inventory
+	PlayerInputComponent->BindAction("InventoryUp", IE_Pressed, this, &AMasteringCharacter::SelectNextWeapon);
+	PlayerInputComponent->BindAction("InventoryDown", IE_Pressed, this, &AMasteringCharacter::SelectPreviousWeapon);
+
+	PlayerInputComponent->BindAction("ToggleMainMenu", IE_Pressed, this, &AMasteringCharacter::ToggleMainMenu);
+}
+
+void AMasteringCharacter::OnFire()
+{
+	// try and fire a projectile
+	if (GetEquippedWeapon() != nullptr)
+	{
+		UAnimInstance* AnimInstance = Mesh1P->GetAnimInstance();
+		GetEquippedWeapon()->Fire(GetControlRotation(), AnimInstance, Inventory);
+	}
+}
+
+void AMasteringCharacter::OnResetVR()
+{
+	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+}
+
+void AMasteringCharacter::BeginTouch(const ETouchIndex::Type FingerIndex, const FVector Location)
+{
+	if (TouchItem.bIsPressed == true)
+	{
+		return;
+	}
+	TouchItem.bIsPressed = true;
+	TouchItem.FingerIndex = FingerIndex;
+	TouchItem.Location = Location;
+	TouchItem.bMoved = false;
+}
+
+void AMasteringCharacter::EndTouch(const ETouchIndex::Type FingerIndex, const FVector Location)
+{
+	if (TouchItem.bIsPressed == false)
+	{
+		return;
+	}
+	if ((FingerIndex == TouchItem.FingerIndex) && (TouchItem.bMoved == false))
+	{
+		OnFire();
+	}
+	TouchItem.bIsPressed = false;
+}
+
+void AMasteringCharacter::ToggleMainMenu()
+{
+	AMasteringHUD* HUD = Cast<AMasteringHUD>(CastChecked<APlayerController>(GetController())->GetHUD());
+	if (HUD != nullptr)
+	{
+		HUD->ToggleMainMenu();
+	}
+}
+
+void AMasteringCharacter::SetInventory(UMasteringInventory* Inv)
+{
+	Inventory = Inv;
+	InitializeInventoryHUD();
+}
+
+void AMasteringCharacter::InitializeInventoryHUD()
+{
+	APlayerController* player = CastChecked<APlayerController>(GetController());
+
+	AMasteringHUD* HUD = Cast<AMasteringHUD>(player->GetHUD());
+
+	if (HUD != nullptr)
+	{
+		HUD->InitializeInventory(Inventory);
+	}
+}
+
+//Commenting this section out to be consistent with FPS BP template.
+//This allows the user to turn without using the right virtual joystick
+
+//void AMasteringCharacter::TouchUpdate(const ETouchIndex::Type FingerIndex, const FVector Location)
+//{
+//	if ((TouchItem.bIsPressed == true) && (TouchItem.FingerIndex == FingerIndex))
+//	{
+//		if (TouchItem.bIsPressed)
+//		{
+//			if (GetWorld() != nullptr)
+//			{
+//				UGameViewportClient* ViewportClient = GetWorld()->GetGameViewport();
+//				if (ViewportClient != nullptr)
+//				{
+//					FVector MoveDelta = Location - TouchItem.Location;
+//					FVector2D ScreenSize;
+//					ViewportClient->GetViewportSize(ScreenSize);
+//					FVector2D ScaledDelta = FVector2D(MoveDelta.X, MoveDelta.Y) / ScreenSize;
+//					if (FMath::Abs(ScaledDelta.X) >= 4.0 / ScreenSize.X)
+//					{
+//						TouchItem.bMoved = true;
+//						float Value = ScaledDelta.X * BaseTurnRate;
+//						AddControllerYawInput(Value);
+//					}
+//					if (FMath::Abs(ScaledDelta.Y) >= 4.0 / ScreenSize.Y)
+//					{
+//						TouchItem.bMoved = true;
+//						float Value = ScaledDelta.Y * BaseTurnRate;
+//						AddControllerPitchInput(Value);
+//					}
+//					TouchItem.Location = Location;
+//				}
+//				TouchItem.Location = Location;
+//			}
+//		}
+//	}
+//}
+
+void AMasteringCharacter::MoveForward(float Value)
+{
+	if (Value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorForwardVector(), Value);
+	}
+}
+
+void AMasteringCharacter::MoveRight(float Value)
+{
+	if (Value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorRightVector(), Value);
+	}
+}
+
+void AMasteringCharacter::TurnAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AMasteringCharacter::LookUpAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AMasteringCharacter::SelectNextWeapon()
+{
+	Inventory->SelectNextWeapon();
+}
+
+void AMasteringCharacter::SelectPreviousWeapon()
+{
+	Inventory->SelectPreviousWeapon();
+}
+
+bool AMasteringCharacter::EnableTouchscreenMovement(class UInputComponent* PlayerInputComponent)
+{
+	if (FPlatformMisc::SupportsTouchInput() || GetDefault<UInputSettings>()->bUseMouseForTouch)
+	{
+		PlayerInputComponent->BindTouch(EInputEvent::IE_Pressed, this, &AMasteringCharacter::BeginTouch);
+		PlayerInputComponent->BindTouch(EInputEvent::IE_Released, this, &AMasteringCharacter::EndTouch);
+
+		//Commenting this out to be more consistent with FPS BP template.
+		//PlayerInputComponent->BindTouch(EInputEvent::IE_Repeat, this, &AMasteringCharacter::TouchUpdate);
+		return true;
+	}
+	
+	return false;
+}
+
+void AMasteringCharacter::EquipWeapon(TSubclassOf<class AMasteringWeapon> Weapon)
+{
+	UWorld *World = GetWorld();
+	if (World == nullptr)
+		return;
+
+	if (EquippedWeaponActor != nullptr)
+	{
+		// if already equipped, we have nothing else to do
+		if (EquippedWeaponActor->IsA(Weapon))
+			return;
+
+		World->DestroyActor(EquippedWeaponActor);
+	}
+
+	const FRotator SpawnRotation = GetActorRotation();
+	const FVector SpawnLocation = GetActorLocation();
+	FActorSpawnParameters ActorSpawnParams;
+	ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	ActorSpawnParams.Owner = this;
+
+	EquippedWeaponActor = Cast<AMasteringWeapon>(World->SpawnActor(Weapon, &SpawnLocation, &SpawnRotation, ActorSpawnParams));
+	if (EquippedWeaponActor != nullptr)
+	{
+		//Attach gun mesh component to skeleton
+		EquippedWeaponActor->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	}
+}
+
+void AMasteringCharacter::PlayFootstepSound()
+{
+	FVector startPos = GetActorLocation();
+	FVector endPos = startPos - FVector(0.0f, 0.0f, 200.0f); // 2m down
+
+	FCollisionQueryParams queryParams;
+	queryParams.AddIgnoredActor(this);
+	queryParams.bTraceComplex = true;
+	queryParams.bReturnPhysicalMaterial = true;
+	FHitResult hitOut;
+
+	bool bHit = GetWorld()->LineTraceSingleByProfile(hitOut, startPos, endPos, TEXT("IgnoreOnlyPawn"));
+
+	if (bHit)
+	{
+		EPhysicalSurface surfHit = SurfaceType_Default;
+
+		if (hitOut.Component->GetBodyInstance() != nullptr && hitOut.Component->GetBodyInstance()->GetSimplePhysicalMaterial() != nullptr)
+		{
+			surfHit = hitOut.Component->GetBodyInstance()->GetSimplePhysicalMaterial()->SurfaceType;
+		}
+
+		if (hitOut.PhysMaterial != nullptr)
+		{
+			surfHit = hitOut.PhysMaterial->SurfaceType;
+		}
+		USoundCue* cueToPlay = nullptr;
+		for (auto physSound : FootstepSounds)
+		{
+			if (physSound.SurfaceType == surfHit)
+			{
+				cueToPlay = physSound.SoundCue;
+				break;
+			}
+		}
+
+		if (cueToPlay != nullptr)
+		{
+			UGameplayStatics::PlaySoundAtLocation(this, cueToPlay, hitOut.Location);
+		}
+	}
+}
